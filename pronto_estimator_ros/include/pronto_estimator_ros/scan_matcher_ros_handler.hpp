e4f08558263a1a6c30f4e74a912f4b103e7b5898
Marco Camurri <mcamurri@robots.ox.ac.uk>
2018-12-15 10:26:11 +0000

    implemented scan and init for ros

---
+++ pronto_estimator_ros/include/pronto_estimator_ros/scan_matcher_ros_handler.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <Eigen/Dense>
+#include <pronto_estimator_core/rbis_update_interface.hpp>
+#include <pronto_estimator_core/mav_state_est.hpp>
+#include <pronto_estimator_core/scan_matcher_module.hpp>
+#include <ros/node_handle.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/LinearMath/Quaternion.h>
+namespace  MavStateEst {
+
+class ScanMatcherHandler : SensingModule<nav_msgs::Odometry>{
+public:
+  ScanMatcherHandler(ros::NodeHandle& nh);
+
+  RBISUpdateInterface * processMessage(const nav_msgs::Odometry * msg,
+                                       MavStateEstimator* state_estimator) override;
+
+  bool processMessageInit(const nav_msgs::Odometry *msg,
+                          const std::map<std::string, bool> &sensor_initialized,
+                          const RBIS &default_state,
+                          const RBIM &default_cov,
+                          RBIS &init_state,
+                          RBIM &init_cov) override;
+
+protected:
+  ScanMatcherModule scan_matcher_module_;
+  PoseMeasurement pose_meas_;
+  ros::NodeHandle& nh_;
+  tf::Quaternion tf_q;
+  void poseMeasurementFromROS(const nav_msgs::Odometry& ros_msg,
+                              PoseMeasurement& msg);
+
+};
+
+
+} // namespace MavStateEst
---
+++ pronto_estimator_ros/src/scan_matcher_ros_handler.cpp
@@ -0,0 +1,143 @@
+#include "pronto_estimator_ros/scan_matcher_ros_handler.hpp"
+#include <string>
+#include <tf_conversions/tf_eigen.h>
+
+namespace MavStateEst {
+
+using ScanMatchingMode = ScanMatcherModule::ScanMatchingMode;
+
+ScanMatcherHandler::ScanMatcherHandler(ros::NodeHandle& nh) : nh_(nh)
+{
+    ScanMatchingMode mode;
+    Eigen::VectorXi z_indices;
+    Eigen::MatrixXd cov_scan_match;
+
+    std::string prefix = "scan_matcher/";
+    std::string mode_str;
+
+    if(!nh_.getParam(prefix + "mode", mode_str)){
+        ROS_WARN("Couldn't get param \"mode\". Using MODE_POSITION by default.");
+        mode_str = "position";
+    }
+
+  if (mode_str.compare("position") == 0) {
+    mode = ScanMatchingMode::MODE_POSITION;
+    std::cout << "Scan matcher will provide position measurements." << std::endl;
+  }
+  else if (mode_str.compare("position_yaw") == 0) {
+    mode = ScanMatchingMode::MODE_POSITION_YAW;
+    std::cout << "Scan matcher will provide position and yaw measurements." << std::endl;
+  }
+  else if (mode_str.compare("velocity") == 0) {
+    mode = ScanMatchingMode::MODE_VELOCITY;
+    std::cout << "Scan matcher will provide velocity measurements." << std::endl;
+  }
+  else if (mode_str.compare("velocity_yaw") == 0) {
+    mode = ScanMatchingMode::MODE_VELOCITY_YAW;
+    std::cout << "Scan matcher will provide velocity and yaw measurements." << std::endl;
+  }
+  else if (mode_str.compare("yaw") == 0) {
+    mode = ScanMatchingMode::MODE_YAW;
+    std::cout << "Scan matcher will provide yaw measurements." << std::endl;
+  }
+  else {
+    mode = ScanMatchingMode::MODE_POSITION;
+    ROS_WARN("Couldn't get param \"mode\". Using MODE_POSITION by default.");
+  }
+  Eigen::VectorXd R_scan_match;
+
+  if (mode == ScanMatchingMode::MODE_POSITION || mode == ScanMatchingMode::MODE_VELOCITY) {
+    z_indices.resize(3);
+    R_scan_match.resize(3);
+  }
+  else if (mode == ScanMatchingMode::MODE_YAW) {
+    z_indices.resize(1);
+    R_scan_match.resize(1);
+  }
+  else {
+    z_indices.resize(4); // Use yaw measurements too.
+    R_scan_match.resize(4);
+  }
+
+  // Initialize covariance matrix based on mode.
+  if (mode == ScanMatchingMode::MODE_POSITION || mode == ScanMatchingMode::MODE_POSITION_YAW)
+  {
+    double r_scan_match_pxy = 0;
+    if(!nh_.getParam(prefix + "r_pxy", r_scan_match_pxy)){
+        ROS_WARN("Couldn't get param \"r_pxy\". Using zero as default.");
+    }
+    double r_scan_match_pz = 0;
+    if(!nh_.getParam(prefix + "r_pz", r_scan_match_pz)){
+        ROS_WARN("Couldn't get param \"r_pz\". Using zero as default.");
+    }
+    R_scan_match(0) = std::pow(r_scan_match_pxy, 2); // Cleaner way?
+    R_scan_match(1) = std::pow(r_scan_match_pxy, 2);
+    R_scan_match(2) = std::pow(r_scan_match_pz , 2);
+    z_indices.head<3>() = eigen_utils::RigidBodyState::positionInds();
+  }
+  else if (mode == ScanMatchingMode::MODE_YAW) {
+    double r_scan_match_yaw = 0;
+    if(!nh_.getParam(prefix + "r_yaw", r_scan_match_yaw)){
+        ROS_WARN("Couldn't get param \"r_yaw\". Using zero as default.");
+    }
+    R_scan_match(0) = std::pow(r_scan_match_yaw * M_PI / 180.0, 2);
+    z_indices(0) = RBIS::chi_ind + 2; // z component only
+  }
+  else {
+    double r_scan_match_vxy = 0;
+    double r_scan_match_vz = 0;
+    if(!nh_.getParam(prefix + "r_vxy", r_scan_match_vxy)){
+        ROS_WARN("Couldn't get param \"r_vxy\". Using zero as default.");
+    }
+    if(!nh_.getParam(prefix + "r_vz", r_scan_match_vz)){
+        ROS_WARN("Couldn't get param \"r_vz\". Using zero as default.");
+    }
+    R_scan_match(0) = std::pow(r_scan_match_vxy, 2); // Cleaner way?
+    R_scan_match(1) = std::pow(r_scan_match_vxy, 2);
+    R_scan_match(2) = std::pow(r_scan_match_vz , 2);
+    z_indices.head<3>() = eigen_utils::RigidBodyState::velocityInds();
+  }
+
+  if (mode == ScanMatchingMode::MODE_POSITION_YAW || mode == ScanMatchingMode::MODE_VELOCITY_YAW) {
+    double r_scan_match_yaw = 0;
+    if(!nh_.getParam(prefix + "r_yaw", r_scan_match_yaw)){
+        ROS_WARN("Couldn't get param \"r_yaw\". Using zero as default.");
+    }
+    R_scan_match(3) = std::pow(r_scan_match_yaw * M_PI / 180.0, 2);
+    z_indices(3) = RBIS::chi_ind + 2; // z component only
+  }
+
+  cov_scan_match = R_scan_match.asDiagonal();
+
+  scan_matcher_module_ = ScanMatcherModule(mode, z_indices, cov_scan_match);
+}
+
+RBISUpdateInterface * ScanMatcherHandler::processMessage(const nav_msgs::Odometry * msg,
+                                                         MavStateEstimator* state_estimator)
+{
+    // convert LCM to generic format
+    pose_meas_.utime = msg->header.stamp.toNSec() / 1000;
+    pose_meas_.linear_vel << msg->twist.twist.linear.x,
+                             msg->twist.twist.linear.y,
+                             msg->twist.twist.linear.z;
+
+    pose_meas_.pos << msg->pose.pose.position.x,
+                      msg->pose.pose.position.y,
+                      msg->pose.pose.position.z;
+    tf::Quaternion tf_q;
+    tf::quaternionMsgToTF(msg->pose.pose.orientation, tf_q);
+    tf::quaternionTFToEigen(tf_q, pose_meas_.orientation);
+    return scan_matcher_module_.processMessage(&pose_meas_,state_estimator);
+}
+
+bool ScanMatcherHandler::processMessageInit(const nav_msgs::Odometry *msg,
+                                            const std::map<std::string, bool> &sensor_initialized,
+                                            const RBIS &default_state,
+                                            const RBIM &default_cov,
+                                            RBIS &init_state,
+                                            RBIM &init_cov)
+{
+    return true;
+}
+
+} // namespace MavStateEst
