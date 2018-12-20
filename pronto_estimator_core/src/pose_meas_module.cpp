15bedc6c075d01145184812f1849b4cd4e36d665
Marco Camurri <mcamurri@robots.ox.ac.uk>
2018-12-20 18:31:11 +0000

    migrate pose_meas module from motion_estimate

---
+++ pronto_estimator_core/src/pose_meas_module.cpp
@@ -0,0 +1,94 @@
+#include "pronto_estimator_core/pose_meas_module.hpp"
+
+namespace MavStateEst {
+
+PoseMeasModule::PoseMeasModule(const PoseMeasConfig &cfg) :
+    mode(cfg.mode), no_corrections(cfg.number_of_corrections)
+{
+    if (mode == PoseMeasMode::MODE_POSITION) {
+        z_indices = RBIS::positionInds();
+        z_meas.resize(3);
+        cov_pose_meas.resize(3, 3);
+        cov_pose_meas = std::pow(cfg.r_pose_meas_xyz, 2) * Eigen::Matrix3d::Identity();
+    }
+    else if(mode == PoseMeasMode::MODE_POSITION_ORIENT) {
+        cov_pose_meas.resize(6, 6);
+        cov_pose_meas.topLeftCorner<3, 3>() = std::pow(cfg.r_pose_meas_xyz, 2) * Eigen::Matrix3d::Identity();
+        cov_pose_meas.bottomRightCorner<3, 3>() = std::pow((cfg.r_pose_meas_chi) * M_PI / 180.0, 2) * Eigen::Matrix3d::Identity();
+        z_meas.resize(6);
+        z_indices.resize(6);
+        z_indices.head<3>() = RBIS::positionInds();
+        z_indices.tail<3>() = RBIS::chiInds();
+    }
+}
+
+RBISUpdateInterface* PoseMeasModule::processMessage(const PoseMeasurement *msg,
+                                                    MavStateEstimator *est)
+{
+    // If we have created no_corrections, go silent afterwards
+    no_corrections--;
+
+    if (no_corrections == 1) {
+      std::cout << "Finished making PoseMeas corrections" << std::endl;
+    }
+
+    if (no_corrections <= 0){
+        // quietly return invalid update if the max number of corrections has
+        // been applied
+      return NULL;
+    }
+
+    // filter out quasi-zero corrections
+    if ((msg->pos.array().abs() < 1e-5).all()){
+      return NULL;
+    }
+
+    if (mode == PoseMeasMode::MODE_POSITION) {
+      return new RBISIndexedMeasurement(z_indices,
+                                        msg->pos,
+                                        cov_pose_meas,
+                                        RBISUpdateInterface::pose_meas,
+                                        msg->utime);
+    }
+    else {
+      z_meas.head<3>() = msg->pos;
+      return new RBISIndexedPlusOrientationMeasurement(z_indices,
+                                                       z_meas,
+                                                       cov_pose_meas,
+                                                       msg->orientation,
+                                                       RBISUpdateInterface::pose_meas,
+                                                       msg->utime);
+    }
+
+}
+
+bool PoseMeasModule::processMessageInit(const PoseMeasurement *msg,
+                                        const std::map<std::string, bool> &sensor_initialized,
+                                        const RBIS &default_state,
+                                        const RBIM &default_cov,
+                                        RBIS &init_state,
+                                        RBIM &init_cov)
+{
+    init_state.utime = msg->utime;
+    init_state.position() = msg->pos;
+    init_cov.block<3, 3>(RBIS::position_ind, RBIS::position_ind) = cov_pose_meas.topLeftCorner<3, 3>();
+
+    if(mode == PoseMeasMode::MODE_POSITION_ORIENT)
+    {
+        init_state.orientation() = msg->orientation;
+        init_cov.block<3, 3>(RBIS::chi_ind, RBIS::chi_ind) = cov_pose_meas.bottomRightCorner<3, 3>();
+    }
+
+
+    Eigen::Vector3d init_rpy_deg = (init_state.getEulerAngles())*180.0 / M_PI;
+
+    std::cout << "Initialized position using a pose_t at xyz:"
+              << init_state.position().transpose() << std::endl;
+
+    std::cout << "Initialized orientation using a pose_t at rpy:"
+              << init_rpy_deg.transpose() << std::endl;
+
+    return true;
+}
+} // namespace MavStateEst
+
---
+++ pronto_estimator_lcm/src/pose_meas_handler.cpp
@@ -0,0 +1,72 @@
+#include "pronto_estimator_lcm/pose_meas_handler.hpp"
+
+using namespace Eigen;
+
+namespace MavStateEst {
+
+PoseMeasHandler::PoseMeasHandler(BotParam * param)
+{
+    PoseMeasConfig cfg;
+    char* mode_str = bot_param_get_str_or_fail(param, "state_estimator.pose_meas.mode");
+
+    if (strcmp(mode_str, "position") == 0) {
+        cfg.mode = PoseMeasMode::MODE_POSITION;
+        std::cout << "PoseMeas will provide position measurements." << std::endl;
+    }
+    else if (strcmp(mode_str, "position_orient") == 0) {
+        cfg.mode = PoseMeasMode::MODE_POSITION_ORIENT;
+        std::cout << "PoseMeas will provide position and orientation measurements." << std::endl;
+    }
+    else {
+        cfg.mode = PoseMeasMode::MODE_POSITION;
+        std::cout << "Unrecognized PoseMeas mode. Using position mode by default." << std::endl;
+    }
+
+    cfg.number_of_corrections = bot_param_get_int_or_fail(param, "state_estimator.pose_meas.no_corrections");
+    std::cout << "Apply " << cfg.number_of_corrections << " PoseMeas corrections before going silent." << std::endl;
+
+    free(mode_str);
+    cfg.r_pose_meas_xyz = bot_param_get_double_or_fail(param, "state_estimator.pose_meas.r_xyz");
+    cfg.r_pose_meas_chi = bot_param_get_double_or_fail(param, "state_estimator.pose_meas.r_chi");
+
+    pose_module_.reset(new PoseMeasModule(cfg));
+}
+
+
+RBISUpdateInterface * PoseMeasHandler::processMessage(const bot_core::pose_t * msg, MavStateEstimator* state_estimator)
+{
+    poseMeasurementFromLCM(*msg, pose_msg_);
+    return pose_module_->processMessage(&pose_msg_,
+                                        state_estimator);
+}
+
+bool PoseMeasHandler::processMessageInit(const bot_core::pose_t * msg,
+                                         const std::map<std::string, bool> & sensors_initialized,
+                                         const RBIS & default_state,
+                                         const RBIM & default_cov,
+                                         RBIS & init_state,
+                                         RBIM & init_cov)
+{
+
+    poseMeasurementFromLCM(*msg, pose_msg_);
+    return pose_module_->processMessageInit(&pose_msg_,
+                                            sensors_initialized,
+                                            default_state,
+                                            default_cov,
+                                            init_state,
+                                            init_cov);
+}
+
+void PoseMeasHandler::poseMeasurementFromLCM(const bot_core::pose_t &lcm_msg,
+                                             PoseMeasurement &msg)
+{
+    // convert LCM to generic format
+    msg.utime = lcm_msg.utime;
+    msg.linear_vel = Eigen::Map<const Eigen::Vector3d>(lcm_msg.vel);
+    msg.angular_vel = Eigen::Map<const Eigen::Vector3d>(lcm_msg.rotation_rate);
+    msg.pos = Eigen::Map<const Eigen::Vector3d>(lcm_msg.pos);
+    eigen_utils::botDoubleToQuaternion(msg.orientation, lcm_msg.orientation);
+}
+
+} // namespace MavStateEst
+
