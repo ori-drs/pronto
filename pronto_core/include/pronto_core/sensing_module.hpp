7d5d217cb328670938160d8f60d6b63a32c90998
Marco Camurri <mcamurri@robots.ox.ac.uk>
2019-07-15 16:57:10 +0100

    experimenting variadic templates
    see
    https://stackoverflow.com/questions/56995453/generate-one-method-per-type-from-variadic-class-template

--- pronto_core/include/pronto_core/sensing_module.hpp
+++ pronto_core/include/pronto_core/sensing_module.hpp
@@ -3,13 +3,13 @@
 #include "pronto_core/state_est.hpp"
 
 namespace pronto {
-template <class MessageT>
+template <class MessageT, class...>
 class SensingModule {
 public:
     virtual ~SensingModule() {}
 
     virtual RBISUpdateInterface* processMessage(const MessageT* msg,
-                                                StateEstimator* est = NULL) = 0;
+                                                StateEstimator* est = nullptr) = 0;
 
     virtual bool processMessageInit(const MessageT* msg,
                                     const std::map<std::string,bool>& sensor_initialized,
@@ -18,5 +18,36 @@ public:
                                     RBIS & init_state,
                                     RBIM & init_cov) = 0;
 
+  inline virtual void processSecondaryMessage() {}
+
+};
+
+
+template <class MessageT, class SecondaryMsgT>
+class SensingModule<MessageT, SecondaryMsgT> {
+public:
+  virtual ~SensingModule() {}
+
+  virtual RBISUpdateInterface* processMessage(const MessageT* msg,
+                                              StateEstimator* est = nullptr) = 0;
+
+  virtual bool processMessageInit(const MessageT* msg,
+                                  const std::map<std::string,bool>& sensor_initialized,
+                                  const RBIS & default_state,
+                                  const RBIM & default_cov,
+                                  RBIS & init_state,
+                                  RBIM & init_cov) = 0;
+
+  virtual void processSecondaryMessage(const SecondaryMsgT& msg) = 0;
+};
+
+template <class MessageT, class SecondaryMsgT, class ... MoreMsgTs>
+class SensingModule<MessageT, SecondaryMsgT, MoreMsgTs ...> :
+public SensingModule<MessageT, MoreMsgTs ...>
+{
+public:
+  virtual ~SensingModule() {}
+  using SensingModule<MessageT, MoreMsgTs...>::processSecondaryMessage;
+  virtual void processSecondaryMessage(const SecondaryMsgT& msg) = 0;
 };
 }
