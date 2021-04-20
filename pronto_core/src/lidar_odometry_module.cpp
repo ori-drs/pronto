47e79481f779855b4dc27739f2d97bb5491f5f5a
Russell Buchanan <russell@robots.ox.ac.uk>
2021-04-20 08:58:16 +0100

    Make lidar odometry POSITION mode in world frame

--- pronto_core/src/lidar_odometry_module.cpp
+++ pronto_core/src/lidar_odometry_module.cpp
@@ -72,7 +72,8 @@ Update* LidarOdometryModule::processMessage(const LidarOdometryUpdate *msg,
 #endif
     // at this point, the mode is either position or position_orient
     if (mode_ == LidarOdometryMode::POSITION) {
-      z_meas.head<3>() = t1_body_vo_.translation();
+      z_meas.head<3>() = t0_body_filter_.translation() + msg->relative_pose.translation();
+      // z_meas.head<3>() = t1_body_vo_.translation();
       std::cerr << "SENDING LIDAR ODOMETRY POSITION UPDATE" << std::endl;
       return new RBISIndexedMeasurement(RigidBodyState::positionInds(),
                                         z_meas,
